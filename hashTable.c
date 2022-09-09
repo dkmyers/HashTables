a1a6f52a2120b4352fbfd5b853b9e4982306daa8
Damon Myers <dkmyers432@gmail.com>
2022-09-09 18:08:51 -0400

    Add files via upload
    Expanded the basic hash functionality into a proper table, with initialization and deletion functions.

--- hashTable.c
+++ hashTable.c
@@ -1,7 +1,15 @@
 #include "hashTables.h"
 
 int main(void) {
-	struct book library[HASH_TABLE_SIZE];
+	//Set up hash table and initialize values to NULL
+	initializeHashTable();
 
+	printf("Does My Dentist Want to Eat My Brain -> %d\n", hashFunction("Does My Dentist Want to Eat My Brain"));
+	printf("Of Ice and Ent -> %d\n", hashFunction("Of Ice and Ent"));
+	printf("Does My Dentist Want to Eat My Brain -> %d\n", hashFunction("Does My Dentist Want to Eat My Brain"));
+
+
+	//free the memory used to store the hash table
+	deleteHashTable();
 	return 0;
 }
--- hashTables.h
+++ hashTables.h
@@ -2,29 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-//define enumerators used in these libraries
+//define constants and data structures used in these files
 enum {
 	BOOK_NAME_LENGTH = 255,
-	HASH_TABLE_SIZE = 10
+	HASH_TABLE_SIZE = 2048
 };
-
 
 struct book {
 	int quantity;
 	char* name;
 };
 
+static struct book* library[HASH_TABLE_SIZE];
+
+//declare functions used in these files
 int hashFunction(char* bookName);
 struct book* addNewBook(char* bookName);
 int incrementBook(struct book* givenBook);
+
+//define functions used in these files
+void initializeHashTable() {
+	for (int i = 0; i < HASH_TABLE_SIZE; i++) {
+		library[i] = NULL;
+	}
+}
+
+void deleteHashTable() {
+	for (int i = 0; i < HASH_TABLE_SIZE; i++) {
+		deleteBook(library[i]);
+	}
+}
 
 //returns hash of book's name
 int hashFunction(char* bookName) {
 	int bookNameLength = strnlen_s(bookName, BOOK_NAME_LENGTH);
 	int hashVal = 0;
 	for (int i = 0; i < bookNameLength; i++) {
-		hashVal += bookName[i];
-		hashVal = hashVal * bookName[i] % HASH_TABLE_SIZE;
+		hashVal += bookName[i] * i;
+		hashVal = hashVal % HASH_TABLE_SIZE;
 	}
 	return hashVal;
 }
@@ -40,6 +55,12 @@ struct book* addNewBook(char* bookName) {
 		newBook->quantity = 1;
 	}
 	return newBook;
+}
+
+void deleteBook(struct book* givenBook) {
+	if (givenBook) {
+		free(givenBook);
+	}
 }
 
 //increases the quantity of a book in the library by 1
