23af61a93d5fd636da0b4c323eb42421bbf2372f
Damon Myers <dkmyers432@gmail.com>
2022-09-16 18:17:03 -0400

    Add files via upload
    Included proper collision handling, then tested that functionality after accidentally finding a collision with book names "This is a test" and "You passed the test"

--- hashTable.c
+++ hashTable.c
@@ -17,19 +17,30 @@ int main(void) {
 	addBook("Castles and Sieges");
 
 	printLibrary();
-	
+	addBook("You passed the test");
+	addBook("You passed the test");
+	addBook("You passed the test");
 	addBook("This is a test");
+	addBook("Tim");
+	addBook("Joe");
+	addBook("J");
+	addBook("K");
+	addBook("ABC");
+	addBook("ACD");
 	addBook("This is a test");
 	addBook("This is a test");
 	addBook("Does My Dentist Want to Eat My Brain");
 	decBook("Does My Dentist Want to Eat My Brain");
 	decBook("Does My Dentist Want to Eat My Brain");
 	decBook("Does My Dentist Want to Eat My Brain");
-
+	decBook("You passed the test");
+	decBook("This is a test");
+	decBook("This is a test");
 	printLibrary();
-
-	printBook("You passed the test");
-	printBook("This is a test");
+	
+	//printf("%d | %d", hashFunction("You passed the test"), hashFunction("This is a test"));
+	
+	//printBook("This is a test");
 
 	//free the memory used to store the hash table
 	deleteHashTable();
--- hashTables.h
+++ hashTables.h
@@ -21,7 +21,7 @@ int hashFunction(char* bookName);
 struct book* addNewBook(char* bookName);
 void incrementBookByObject(struct book* givenBook);
 void decrementBookByObject(struct book* givenBook);
-void addBook(char* bookName);
+int addBook(char* bookName);
 void decBook(char* bookName);
 void deleteBook(struct book* givenBook);
 void printLibrary(void);
@@ -45,8 +45,8 @@ int hashFunction(char* bookName) {
 	int bookNameLength = strnlen_s(bookName, BOOK_NAME_LENGTH);
 	int hashVal = 0;
 	for (int i = 0; i < bookNameLength; i++) {
-		hashVal += (bookName[i] + i);
-		hashVal = hashVal % HASH_TABLE_SIZE;
+		hashVal += (bookName[i]);
+		hashVal = (hashVal*bookName[i]) % HASH_TABLE_SIZE;
 
 	}
 	return hashVal;
@@ -81,7 +81,11 @@ void decrementBookByObject(struct book* givenBook) {
 	givenBook->quantity -= 1;
 }
 
-void addBook(char* bookName) {
+int addBook(char* bookName) {
+	if (!bookName) {
+		return 0;
+	}
+
 	//given the name of the book being searched, find its location in library array
 	int hashResult = hashFunction(bookName);
 	struct book* bookObj = library[hashResult];
@@ -90,11 +94,37 @@ void addBook(char* bookName) {
 	if (!bookObj) {
 		bookObj = addNewBook(bookName);
 		library[hashResult] = bookObj;
+		return 0;
 	}
 	else {
-		incrementBookByObject(bookObj);
-	}
+		//If this book has been initialized already, check if it's the same book or if this is a collision
+		if (strcmp(bookName, library[hashResult]->name) == 0) {
+			//The given name is the same as the book already in this position, which means we just increment the quantity
+			incrementBookByObject(bookObj);
+		}
+		else {
+			int index = 0;
+			//The names are different, so we have a collision
+			//Iterate through the table until an open space is available
+			for (int i = 0; i < HASH_TABLE_SIZE; i++) {
+				index = (hashResult + i) % HASH_TABLE_SIZE;
+				if (!library[index]) {
+					//space is uninitialized
+					//Create a new object for this position, rather than use the bookObj one previously
+					library[index] = addNewBook(bookName);
+					return 0;
+				}
 
+				if ((strcmp(bookName, library[index]->name)) == 0) {
+					//space is initialized, but containing this book
+					incrementBookByObject(library[index]);
+					return 0;
+				}
+			}
+			return -1;
+		}
+	}
+	return -1;
 }
 
 void decBook(char* bookName) {
@@ -103,7 +133,7 @@ void decBook(char* bookName) {
 	struct book* bookObj = library[hashResult];
 
 	//check that this book has been initialized already
-	if (bookObj) {
+	if (bookObj && (strcmp(bookName, bookObj->name) == 0)) {
 		decrementBookByObject(bookObj);
 		//check if book is gone entirely, in which case its spot in library is removed
 		if (bookObj->quantity < 1) {
@@ -111,7 +141,18 @@ void decBook(char* bookName) {
 		}
 	}
 	else {
-		//Book was not found
+		int index = 0;
+		//Book was not found at hashResult
+		//check rest of table, because it may have been included via collision
+		for (int i = 0; i < HASH_TABLE_SIZE; i++) {
+			index = (hashResult + i) % HASH_TABLE_SIZE;
+			if (library[index]) {
+				if ((strcmp(bookName, library[index]->name)) == 0) {
+					//space is initialized, but containing this book
+					decrementBookByObject(library[index]);
+				}
+			}
+		}
 	}
 }
 
@@ -127,10 +168,27 @@ void printLibrary(void) {
 
 void printBook(char* bookName) {
 	int hashResult = hashFunction(bookName);
-	if (library[hashResult]) {
-		printf("\nPosition: %d | Name: %s | Quantity: %d\n", hashResult, library[hashResult]->name, library[hashResult]->quantity);
+
+	//check if the given name matches the library's name for this location
+	if (strcmp(bookName, library[hashResult]) == 0) {
+		if (library[hashResult]) {
+			printf("\nPosition: %d | Name: %s | Quantity: %d\n", hashResult, library[hashResult]->name, library[hashResult]->quantity);
+		}
+		else {
+			//Book was not found
+			printf("\nBook not found: %s", bookName);
+			return;
+		}
 	}
 	else {
-		//Book was not found
+		int index = 0;
+		for (int i = 0; i < HASH_TABLE_SIZE; i++) {
+			//go through entire table until match is found
+			index = (hashResult + i) % HASH_TABLE_SIZE;
+			if (library[index] && (strcmp(bookName, library[index]) == 0)) {
+				//match was found
+				printf("\nPosition: %d | Name: %s | Quantity: %d\n", index, library[index]->name, library[index]->quantity);
+			}
+		}
 	}
 }
